1ae4ee924a092fc916d29d8078737a4c4eafcf76
unknown <lam.pn27022k@gmail.com>
2023-07-03 16:51:28 +0700

    up

---
+++ sim/work/filelist_rtl.f
@@ -0,0 +1,9 @@
++libext+.v                       // Verilog wrapper files
++libext+.vh                      // Verilog wrapper files
++libext+.sv                      // Verilog wrapper files
+
+//  Design Library
++incdir+../../hdl
+-y  ../../hdl
+
+../../hdl/uart.v
